cae42af39a5b6638c4f42f1de6f7eb97295f1b97
Marian <engine2@gmx.de>
2023-07-07 09:17:05 +0200

    Datenbankanbindung geaendert

--- Datenbankanbindung.java
+++ Datenbankanbindung.java
@@ -1,39 +1,51 @@
 import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.*;
 
 public class Datenbankanbindung {
-    private Connection conn;
-
-    public Datenbankanbindung() {
-        /*
-         * https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-usagenotes-connect-drivermanager.html
-         * 
-         */
+    private static Connection conn;
+    private static Datenbankanbindung instance;
+    
+    static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection("jdbc:mysql://172.28.40.54/kino?user=q11&password=dg");
             //conn = DriverManager.getConnection("jdbc:mysql://localhost/kino?user=root&password=");
-            
             System.out.println(conn);
-            
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
     
-    public void filmeAusgeben() {
+    @FunctionalInterface
+    private static interface SQLFunction<P, R> {
+        public R apply(P arg) throws SQLException;
+    }
+    
+    public static <E> List<E> getAllDataInColumn(String column, SQLFunction<ResultSet, E> cons) {
+        var list = new ArrayList<E>();
         try {
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM film");
-            while ( rs.next() ) {
-                System.out.println(rs.getString("name") + ", " + rs.getInt("laenge"));
+            ResultSet rs = st.executeQuery("SELECT * FROM " + column);
+            while (rs.next()) {
+                list.add(cons.apply(rs));
             }
             rs.close();
             st.close();            
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
-        
+        return list;
+    }
+    
+    /**
+     * Holt sich die Filme aus der Datenbank
+     * 
+     * @param name Der Name
+     */
+    public static List<Film> getFilme() {
+        return getAllDataInColumn("film", rs -> new Film(rs.getString("name"), rs.getInt("jahr"), rs.getInt("laenge"), rs.getInt("FSK")));
     }
 
 }
